297cabb7fdd829c6a252bcf41537a5dec9fb1ba8
mifrain <mifrainyt@gmail.com>
2025-01-03 18:28:42 +0300

    Registration

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 
 from app.bookings.router import router as router_booking
+from app.users.router import router as router_users
 
 app = FastAPI()
 
+
+app.include_router(router_users)
 app.include_router(router_booking)
